7125ed7915f521527f14bb84c385ba29cde343dc
JusGabriel <168014809+JusGabriel@users.noreply.github.com>
2025-11-27 23:31:01 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,6 @@ import connection from './database.js';
 connection()
 
 
-server.listen(app.get('port'),()=>{
+app.listen(app.get('port'),()=>{
     console.log(`Server ok on http://localhost:${app.get('port')}`);
 })
